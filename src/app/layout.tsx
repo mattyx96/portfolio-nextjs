9558055099b3235e5b613b974e46252d74187949
Matteo Omicini <matteoomicini.dev@gmail.com>
2025-01-03 15:13:41 +0100

    fix: browser theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,31 +7,33 @@ import React from "react";
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Matteo Omicini's Portfolio",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  }
+    title: "Portfolio",
+    description: "Matteo Omicini's Portfolio",
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    }
 };
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-    <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet"/>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
-            rel="stylesheet"/>
-      <meta name="theme-color" content="#000"/>
-    </head>
-    <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+                rel="stylesheet"/>
+            <meta name="theme-color" content="#000"/>
+            <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
