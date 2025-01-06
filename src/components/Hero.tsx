8fae293cd48c137ec84ffb582c8dc4fd708f82fd
Matteo Omicini <matteoomicini.dev@gmail.com>
2025-01-06 20:04:12 +0100

    feat: hero improvements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,14 +2,15 @@ import {Button, config, Text} from "nebula-ds-react-library";
 import {SectionContent} from "@/components/SectionContent";
 import StarField from "@/components/StarField";
 import {randomPick} from "@/utils";
+import {ChevronDownIcon} from "@heroicons/react/24/solid";
 import {useEffect, useState} from "react";
 
 export const Hero = () => {
     return (
         <StarField backgroundColor={config.lightJsTokens.nbBlack} speedFactor={0.008}>
             <section
-                className="h-screen w-full flex items-center justify-center md:pl-10 lg:pl-5 xl:pl-0 md:!justify-start">
-                <SectionContent className="justify-center !w-full">
+                className="relative h-screen w-full flex items-center justify-center md:pl-10 lg:pl-5 xl:pl-0 md:!justify-start">
+                <SectionContent className="justify-center -translate-y-[7vh] !w-full">
                     <Text component="h1" variant="display1"
                           className="!text-background-contrast-primary-50 text-center md:!text-left">
                         Hi, I'm Matteo
@@ -18,14 +19,18 @@ export const Hero = () => {
                           className="!text-background-contrast-primary-50 text-center md:!text-left">
                         A.k.a. Gary
                     </Text>
-                    <Text variant="header6"
-                          className="!text-background-contrast-primary-50 mt-10 text-center md:!text-left">
+                    <Text variant="body1"
+                          className="!text-background-contrast-primary-50 mt-8 text-center md:!text-left">
                         I’m developing web experiences
                     </Text>
                     <div className="w-full flex justify-center md:!justify-start">
                         <CTA/>
                     </div>
                 </SectionContent>
+                <div
+                    className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-pulse w-6 h-6 text-white">
+                    <ChevronDownIcon/>
+                </div>
             </section>
         </StarField>
     )
